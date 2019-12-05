8069fda09d883b2e4e1c2d0309780dbf355baf07
mor3s <nour.boulahcen@gmail.com>
2019-12-05 08:23:11 +0100

    Add feature extraction function

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from cluster.cluster import Cluster
 import numpy as np
+from tsfresh import extract_features
+from tsfresh import select_features
+from tsfresh.utilities.dataframe_functions import impute
+from tsfresh import extract_relevant_features
 
+
+
+def Extract_features_from_TS(Data, y):
+    extracted_features = extract_features(Data, column_id="id", column_sort="time")
+    impute(extracted_features)
+    features_filtered = select_features(extracted_features, y)
+    features_filtered_direct = extract_relevant_features(timeseries, y,
+                                                     column_id='id', column_sort='time')
+    return extracted_features, features_filtered_direct
+
+    
 if __name__ == "__main__":
     features = np.array([
         1 * np.linspace(0, 100, 100),
