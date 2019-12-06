e00f3d3ced569755c4b0da10505dbb75ed0a684f
QuentinN42 <quentin.lieumont@gmail.com>
2019-12-06 13:50:48 +0100

    direct cluster

--- cluster_direct.py
+++ cluster_direct.py
@@ -4,28 +4,33 @@ import matplotlib.pyplot as plt
 import sklearn
 import random
 from math import sqrt
+import pandas as pd
 
 
-def DTWDistance(s1, s2,w: float = np.inf):
-    DTW={}
+def DTWDistance(s1, s2, w: float = np.inf):
+    DTW = {}
 
-    w = max(w, abs(len(s1)-len(s2)))
+    w = max(w, abs(len(s1) - len(s2)))
 
-    for i in range(-1,len(s1)):
-        for j in range(-1,len(s2)):
-            DTW[(i, j)] = float('inf')
+    for i in range(-1, len(s1)):
+        for j in range(-1, len(s2)):
+            DTW[(i, j)] = float("inf")
     DTW[(-1, -1)] = 0
 
     for i in range(len(s1)):
-        for j in range(max(0, i-w), min(len(s2), i+w)):
-            dist= (s1[i]-s2[j])**2
-            DTW[(i, j)] = dist + min(DTW[(i-1, j)],DTW[(i, j-1)], DTW[(i-1, j-1)])
+        for j in range(max(0, i - w), min(len(s2), i + w)):
+            dist = (s1[i] - s2[j]) ** 2
+            DTW[(i, j)] = dist + min(
+                DTW[(i - 1, j)], DTW[(i, j - 1)], DTW[(i - 1, j - 1)]
+            )
 
-    return sqrt(DTW[len(s1)-1, len(s2)-1])
+    return sqrt(DTW[len(s1) - 1, len(s2) - 1])
+
 
 if __name__ == "__main__":
-	clust = Cluster(data, metric=DTWDistance)
-	clust.print(10)
-
-
-
+    data = np.array(pd.read_csv("data/data1.csv").head(10))[(0, 1, 3, 5, 6, 8), 1:]
+    print(data)
+    print(data.shape)
+    clust = Cluster(data, metric=DTWDistance)
+    clust.print(2)
+    clust.dendogram()
